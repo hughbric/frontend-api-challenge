2c4f60cb141d992df3cce4c3367155032e7ba6ef
Michael Hughes <mhughes36@live.co.uk>
2019-07-01 09:16:00 +0100

    refactor and button functionality

--- interface.js
+++ interface.js
@@ -1,15 +1,26 @@
 window.onload = function() {
   (function listPeeps() {
-    const request = new XMLHttpRequest()
-    request.open('GET', 'https://chitter-backend-api.herokuapp.com/peeps')
+    const request = new XMLHttpRequest();
+    request.open('GET', 'https://chitter-backend-api.herokuapp.com/peeps');
     request.onload = function() {
-      const data = JSON.parse(this.response)
+      const data = JSON.parse(this.response);
 
       data.forEach(peep => {
         const article = Peep.format(peep);
         $('#peep-list').append(article);
       });
-    }
+    };
     request.send();
   }());
+
+  $('#sign-up-button').click(function() {
+    $('#home-page').hide();
+    $('#sign-up-page').show();
+  });
+
+  $('#sign-in-button').click(function() {
+    $('#home-page').hide();
+    $('#sign-in-page').show();
+  });
+
 };
--- spec/assert.js
+++ spec/assert.js
@@ -10,8 +10,7 @@ let assert = {
   isEqual: function(firstItem, secondItem) {
     if (firstItem !== secondItem) {
       throw new Error("Assertion failed: '" + firstItem + "' & '" + secondItem + "' are not equal");
-    }
-    else{
+    } else {
       console.log('%c' + "Test passed, " + firstItem + " is equal to " + secondItem, 'color: green');
     }
   },
